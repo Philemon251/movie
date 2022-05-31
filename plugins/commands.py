a9f408d8e9eaf5486f046388e16f52c304e5a6d1
Godstimep21 <104645270+Godstimep21@users.noreply.github.com>
2022-05-31 12:14:21 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -93,6 +93,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
+            InlineKeyboardButton('Search Here 🔍', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Updates Channel🍿🎬', url='https://t.me/Lordship_Movies')
             ], [
             InlineKeyboardButton('Series Bot 🎥', url='https://t.me/Lordship_series'),
